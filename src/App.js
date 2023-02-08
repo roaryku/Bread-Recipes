9068c8505827836cd0387cea306e32fbd5bef090
Rodika <raryku@yahoo.com>
2023-02-07 19:25:44 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useCallback } from 'react';
+import video from './bread.mp4';
+import icon from './icons8.png'
+import MyRecipesComponent from './MyRecipesComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  const MY_ID = "fbf1aefa";
+  const MY_KEY = "1c7e7a9e2f62ef5ccd608ced66f4e2c3";
+
+  const [mySearch, setMySearch] = useState('');
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState('bread');
+
+  
+    const getNewRecipe = useCallback(async () => {
+      const responce = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
+      const data = await responce.json();
+      console.log(data.hits);
+      setMyRecipes(data.hits);
+    },[wordSubmitted])
+    useEffect(() =>{
+      getNewRecipe();
+  }, [getNewRecipe])
+
+const myRecipeSearch = (e) => {
+  console.log(e.target.value);
+  setMySearch(e.target.value);
 }
 
+const finalSearch = (e) => {
+  e.preventDefault();
+  setWordSubmitted(mySearch);
+}
+
+  return(
+    <div className="App">
+      <div className='container'>
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4"/>
+        </video>
+        <h1>Find your bread recipe</h1>
+    </div>
+      
+      <div className='container'>
+        <form onSubmit = {finalSearch}>
+          <input className='search' placeholder='search...' onChange={myRecipeSearch} value={mySearch}/>
+        </form>
+          <button className='btn'>
+            <img src={icon} width="35px" className='icons' alt='baking'/>
+          </button>
+      </div>
+
+      <div>
+        {myRecipes.map(element => (
+          <MyRecipesComponent 
+          label = {element.recipe.label}
+          cuisine = {element.recipe.cuisineType}
+          image = {element.recipe.image}
+          ingredients = {element.recipe.ingredientLines}
+          calories = {element.recipe.calories}
+          diet = {element.recipe.dietLabels}
+          time = {element.recipe.totalTime}
+          />
+        ))}
+      </div>
+    </div>
+  
+  )
+}
 export default App;
---
+++ src/MyRecipesComponent.js
@@ -0,0 +1,39 @@
+import icon from './double.png';
+
+function MyRecipesComponent({label, image, ingredients, cuisine, calories, diet, time}){
+    return(
+        <div>
+             <div className="container">
+                <img className="tasty" src={image} width="200" alt="breadSearch"/>
+            </div>
+
+            <div className="container">
+                <h2>{label}</h2>
+            </div>
+
+            <div className="container">
+                <h3>{cuisine}</h3>
+            </div>
+
+            <ul className="list">
+                {ingredients.map( ingredient => (
+                    <li><img src={icon} alt='checkBox' className='icon'/>
+                    {ingredient}</li>
+                ))}
+            </ul>
+
+            <div className="container">
+                <p className="par">{diet}</p>
+            </div>
+
+            <div className="container">
+                <p className="par">{calories.toFixed()} calories</p>
+            </div>
+
+            <div className="container">
+                <p className="par">{time} minutes</p>
+            </div>
+        </div>
+    )
+}
+export default MyRecipesComponent;
